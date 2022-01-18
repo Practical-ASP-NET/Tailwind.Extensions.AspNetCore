0c4478d5644e194a989bb89bc3921fb7f8cdf535
Jon Hilton <hilton.jon@gmail.com>
2022-01-18 22:25:40 +0000

    Sorting out release builds

---
+++ Push.ps1
@@ -0,0 +1,16 @@
+# Taken from MediatR.Extensions.DependencyInjection https://github.com/jbogard/MediatR.Extensions.Microsoft.DependencyInjection/
+
+$scriptName = $MyInvocation.MyCommand.Name
+$artifacts = "./artifacts"
+
+if ([string]::IsNullOrEmpty($Env:NUGET_API_KEY)) {
+    Write-Host "${scriptName}: NUGET_API_KEY is empty or not set. Skipped pushing package(s)."
+} else {
+    Get-ChildItem $artifacts -Filter "*.nupkg" | ForEach-Object {
+        Write-Host "$($scriptName): Pushing $($_.Name)"
+        dotnet nuget push $_ --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+        if ($lastexitcode -ne 0) {
+            throw ("Exec: " + $errorMessage)
+        }
+    }
+}
